67ac93cc1ee99a2e930ba48b6fa280138edc5cc4
patrickmulholland <patrick.mulholland@gmx.de>
2013-07-17 15:32:46 +0200

    bugfixing category-error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130712113219) do
+ActiveRecord::Schema.define(:version => 20130712123219) do
 
   create_table "agencies", :force => true do |t|
     t.string   "name"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(:version => 20130712113219) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "category_id"
-    t.integer  "type_id"
     t.integer  "client_id"
     t.string   "tags"
+    t.integer  "type_id"
   end
 
   create_table "likes", :force => true do |t|
